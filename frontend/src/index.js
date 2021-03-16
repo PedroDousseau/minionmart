57d94038388a83d23221ab07b850d286fb5357a6
Pedro Dousseau <pv.dousseau@gmail.com>
2021-03-16 15:07:09 -0300

    Scroll to top on every redirect

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
+      <ScrollToTop/>
       <App />
     </Router>
   </React.StrictMode>,
