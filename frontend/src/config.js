e924aee25605d5be318a07daa3f997354d6ea5f9
Pedro Dousseau <pv.dousseau@gmail.com>
2021-03-16 21:09:13 -0300

    Added and cofigured AWS Amplify

---
+++ frontend/src/config.js
@@ -0,0 +1,10 @@
+const config = {
+    cognito: {
+      REGION: "sa-east-1",
+      USER_POOL_ID: "sa-east-1_9YZQb7QHY",
+      APP_CLIENT_ID: "7uld3nl4lqj3kednsu56u04ftn",
+    },
+  };
+  
+  export default config;
+  
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
 import ScrollToTop from './components/ScrollToTop/ScrollToTop';
+import { Amplify } from 'aws-amplify';
+import config from './config';
+
 
 
 ReactDOM.render(
@@ -21,3 +24,13 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID
+  },
+});
+
