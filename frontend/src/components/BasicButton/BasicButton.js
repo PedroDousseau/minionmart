6daf1547766d0f34bb2647939df4428621099750
Pedro Dousseau <pv.dousseau@gmail.com>
2021-03-16 21:35:11 -0300

    Removed bootstrap from BasicButton

--- frontend/src/components/BasicButton/BasicButton.js
+++ frontend/src/components/BasicButton/BasicButton.js
@@ -1,11 +1,14 @@
 import './BasicButton.css';
 import React from "react";
-import {Button} from 'react-bootstrap';
 
 export default function BasicButton(props) {
   return (
-    <Button size="lg" variant="primary" className="BasicButton_mainContainer" style={{backgroundColor: props.bgColor, color: props.color, width: props.fullWidth ? '100%' : 'max-content'}}>
+    <button 
+    className="BasicButton_mainContainer" 
+    style={{backgroundColor: props.bgColor, color: props.color, width: props.fullWidth ? '100%' : 'max-content'}}
+    onClick = {props.onClick}
+    >
         {props.children}
-    </Button>
+    </button>
   );
 }
