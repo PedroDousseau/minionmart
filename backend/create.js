09ce4dc4a25a174d3223164f0fa954fd9e71b3e2
EC2 Default User <ec2-user@ip-172-31-2-235.sa-east-1.compute.internal>
2021-03-18 04:54:50 +0000

    List and create events

---
+++ backend/create.js
@@ -0,0 +1,23 @@
+import * as uuid from "uuid";
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
+
+export const main = handler(async (event, context) => {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: process.env.tableName,
+    Item: {
+      // The attributes of the item to be created
+      productId: uuid.v1(), // A unique uuid
+      name: data.name, // Parsed from request body
+      img: data.img, // Parsed from request body
+      description: data.description, // Parsed from request body
+      details: data.details, // Parsed from request body
+      createdAt: Date.now(), // Current Unix timestamp
+    },
+  };
+
+  await dynamoDb.put(params);
+
+  return params.Item;
+});
