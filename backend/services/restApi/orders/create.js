f84429755f79c38cefc1e898ba7a34242d9e055d
EC2 Default User <ec2-user@ip-172-31-2-235.sa-east-1.compute.internal>
2021-03-19 21:49:36 +0000

    Checkout stepf + get user orders

--- backend/services/restApi/orders/create.js
+++ backend/services/restApi/orders/create.js
@@ -1,21 +1,29 @@
-import * as uuid from "uuid";
 import handler from "../../../libs/handler-lib";
+import * as uuid from "uuid";
 import dynamoDb from "../../../libs/dynamodb-lib";
 
 export const main = handler(async (event, context) => {
-  const data = JSON.parse(event.body);
+  //const data = JSON.parse(event.body);
+  const data = event;
+
   const params = {
-    TableName: process.env.tableName,
+    TableName: process.env.orderTable,
     Item: {
-      // The attributes of the item to be created
-      orderId: uuid.v1(), // A unique uuid
-      userId: data.name, // Parsed from request body
-      items: data.items,
-      createdAt: Date.now(), // Current Unix timestamp
+      orderId: uuid.v1(),
+      userId: data.userId, // Parsed from request body
+      items: data.items, // Parsed from request body
+      createdAt: Date.now(),
     },
   };
 
-  await dynamoDb.put(params);
-
-  return params.Item;
+  return dynamoDb.put(params)
+  .then(() => {
+    return {
+      emails: [data.userId, "pedro32santos@hotmail.com"],
+      order: data.items
+    };
+  })
+  .catch((err) => {
+    return err.message;
+  });
 });
