0d385eaeed738c7aed91bb01b5c6736597ec64a8
Pedro Dousseau <pv.dousseau@gmail.com>
2021-03-20 00:21:46 -0300

    Added icons + logo

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,10 +1,12 @@
 import React from "react";
 import './Footer.css'
+import logo from '../../assets/logo.svg';
 
 export default function Footer(props) {
   return (
     <div className="Footer_container">
-        <h3>A.U.M</h3>
+      <img src={logo} alt=""/>
+      <p className="Fotter_copyright"> Made by Pedro Dousseau © 2021</p>
     </div>
   );
 } 
